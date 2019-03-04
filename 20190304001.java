a0a2d0625e2f4154e8eace73957830391a89c22a
sofine2000sf2k <47215426+sofine2000sf2k@users.noreply.github.com>
2019-03-04 22:18:58 +0800

    Create 20190304001.java

---
+++ 20190304001.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class App3_7
+{
+  public static void main(String[] args)
+  {   
+    Scanner reader=new Scanner(System.in);
+    int a=reader.nextint();
+    
+    a=a+1;
+    
+    System.out.print(a);
+    System.out.println("hello world");
+  }
+}
